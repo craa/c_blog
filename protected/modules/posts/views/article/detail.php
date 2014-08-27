5253ec8ab396c1ad390247704860c36cfea3222f
craa <412280471@qq.com>
2014-08-28 01:22:12 +0800

    增加多说评论系统

--- protected/modules/posts/views/article/detail.php
+++ protected/modules/posts/views/article/detail.php
@@ -18,6 +18,10 @@
                 <?php echo $article->post_content; ?>
             </div>
         </div>
+        <!-- 多说评论框 start -->
+        <div class="ds-thread" data-thread-key="<?php echo $article->ID; ?>" data-title="<?php echo $article->post_title; ?>" data-url="<?php echo $this->createAbsoluteUrl('', array('id'=>$article->ID));?>"></div>
+        <!-- 多说评论框 end -->
+
     </article>
     <!-- 右侧栏 -->
     <div class="col-sm-3 hidden-xs">
@@ -29,4 +33,18 @@
 <?php $this->widget('ext.ueditor.UeditorParseWidget', array(
             'selector'=>'.article-content', //必选* 要渲染位置的选择器
             'sh_theme'=>'Default', //可选 语法高亮的主题{Default,Django,Eclipse,Emacs,FadeToGrey,MDUltra,Midnight,RDark}
-));
+));?>
+
+<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+        <script type="text/javascript">
+            var duoshuoQuery = {short_name:"crarun"};
+            (function() {
+                var ds = document.createElement('script');
+                ds.type = 'text/javascript';ds.async = true;
+                ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+                ds.charset = 'UTF-8';
+                (document.getElementsByTagName('head')[0]
+                    || document.getElementsByTagName('body')[0]).appendChild(ds);
+            })();
+        </script>
+<!-- 多说公共JS代码 end -->
