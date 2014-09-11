9df4a10ba5a1afb2cbbbcbe0bee8b72dba863009
craa <412280471@qq.com>
2014-09-11 21:14:12 +0800

    修改文件名

--- protected/components/widgets/views/BaiduSearchView.php
+++ protected/components/widgets/views/BaiduSearchView.php
@@ -5,6 +5,7 @@
  * Description:
  */
 ?>
+
 <div class="panel panel-default">
     <div class="panel-body">
         <script type="text/javascript">document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E%3Cscript charset="utf-8" src="http://rp.baidu.com/rp3w/3w.js?sid=7324604176948805817') + '&t=' + (Math.ceil(new Date()/3600000)) + unescape('"%3E%3C/script%3E'));</script>
