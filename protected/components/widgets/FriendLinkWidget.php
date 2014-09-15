e708eede41cd4028d238af770f03f969d5ec48d1
craa <412280471@qq.com>
2014-09-16 01:05:36 +0800

    友链

--- protected/components/widgets/FriendLinkWidget.php
+++ protected/components/widgets/FriendLinkWidget.php
@@ -9,6 +9,7 @@ class FriendLinkWidget extends CWidget
     public function run()
     {
         $cri = new CDbCriteria;
+        $cri->condition = 'link_visible = "Y"';
         $cri->order = 'link_id';
         $friendLinks = Links::model()->findAll($cri);
         $this->render('FriendLinkView',array('friendLinks'=>$friendLinks));
